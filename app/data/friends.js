a5e3d0720f129145591a636a3dbe750dd56be740
azza <azzabdin@gmail.com>
2019-05-07 19:05:06 -0400

    l

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends= [{
+    
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+       "5" ,
+        "3",
+        "4",
+        "2",
+        "5",
+        "4",
+        "3",
+        "5",
+        "3",
+        "1"
+    ]
+  },
+]
+  module.exports=friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+var friends = require("../data/friends.js")
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+
+    });
+    app.post("/api/friends", function (req, res) {
+        var bestfriend = {
+            name: "",
+            photo: "",
+            fdiff: 1000,
+        };
+        var user = req.body;
+        var userscores = user.scores;
+        console.log({"user.scores": user.scores});
+        var diff = 0;
+        for (var i = 0; i < friends.length; i++) {
+            diff = 0;
+            for (var u = 0; u < friends[i].scores[u]; u++) {
+                
+                diff += Math.abs(parseInt(userscores[u]) - parseInt(friends[i].scores[u]));
+                
+                if (diff <= bestfriend.fdiff) {
+                    bestfriend.name = friends[i].name;
+                    bestfriend.photo = friends[i].photo;
+                    bestfriend.fdiff = diff;
+
+                }
+            }
+        
+        }
+        friends.push(user)
+        res.json(bestfriend);
+        
+    
+    });
+}
